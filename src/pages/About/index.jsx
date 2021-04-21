00c1c4b96eca60d2f7ff9e3becece1bf788843b7
Robert Samuel <robert_samuel97@hotmail.com>
2021-04-21 10:11:48 -0600

    About - Add the style to the section

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -24,13 +24,13 @@ const About = () => {
     const renderItems = () => {
         return ITEMS.map(item => {
             return (
-                <div>
-                    <div>
-                        <Icon iconName={item.icon} />
+                <div className="About-item">
+                    <div className="About-firstcol">
+                        <Icon classes="About-icon blue-icon" iconName={item.icon} />
                     </div>
-                    <div>
-                        <span>{item.title}</span>
-                        <span>{item.text}</span>
+                    <div className="About-data">
+                        <span className="About-label">{item.title}</span>
+                        <span className="About-text">{item.text}</span>
                     </div>
                 </div>
             );
@@ -41,22 +41,24 @@ const About = () => {
         <section className="About">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-7 col-xl-6 col-12">
+                        <div className="col-lg-7 col-xl-6 col-12 margin-auto">
                             <div>
-                                <span>Welcome To An Nafi</span>
-                                <h2>About Nafi</h2>
-                            </div>
-                            <p>
-                            Web design and Web development focused on clean & user-friendly experiences, I am experiences about building excellent Websites that improves the lives of those around me.
-                            </p>
-                            <p>
-                            Web design and Web development focused on clean & user-friendly experiences, Websites that improves.
-                            </p>
-                            <div>
-                                {renderItems()}
+                                <div>
+                                    <span className="About-welcome">Welcome To An Nafi</span>
+                                    <h2 className="About-title">About Nafi</h2>
+                                </div>
+                                <p className="About-paragraph">
+                                Web design and Web development focused on clean & user-friendly experiences, I am experiences about building excellent Websites that improves the lives of those around me.
+                                </p>
+                                <p className="About-paragraph">
+                                Web design and Web development focused on clean & user-friendly experiences, Websites that improves.
+                                </p>
+                                <div>
+                                    {renderItems()}
+                                </div>
                             </div>
                         </div>
-                        <div className="col-lg-5 col-xl-6 col-12">
+                        <div className="col-lg-5 col-xl-6 col-12 margin-auto">
                             <img
                                 className="About-image"
                                 src={welcomeImage}
